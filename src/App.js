31d62e6a1547056e54ec5fe1c4261fcd6dbd33bc
ARVIND-RAKA <160499397+ARVIND-RAKA@users.noreply.github.com>
2024-03-17 23:46:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Counter from './components/Counter';
+import Navbar from './components/Navbar';
+import Product from './components/Product';
+import Cart from './components/Cart';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  return (<>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+       
+
+       <Route path='/' element={<Product/>}/>
+       <Route path="/cart" element={<Cart/>}/>
+    </Routes>
+    
+    
+  </BrowserRouter>
+       
+    
+  
+   
+    
+  </>
+    
+  )
 }
 
-export default App;
+export default App
